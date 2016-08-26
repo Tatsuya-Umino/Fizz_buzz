87d830d0a681ab70aa3d9825037b869279e584e7
ryuya.umino <r.umino@tcmobile.jp>
2016-08-26 15:39:08 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+lazy val root = (project in file(".")).
+  settings(
+      name := "Fizz_buzz",
+      version := "1.0",
+      scalaVersion := "2.11.8"
+  )
---
+++ src/main/scala/fizzBuzz.scala
@@ -0,0 +1,34 @@
+import scala.io.StdIn.readLine
+
+object fizzBuzz {
+    def main(args: Array[String]): Unit = {
+
+        while(true){
+            print("数値を入力してください : ")
+            var input = readLine()
+
+            if(input == "fin"){
+                return
+            }
+
+            if (input == "" || !input.matches("^[0-9]*$")){
+                println("整数を入力してください")
+            }else{
+              fizzBuzz(input.toInt)
+            } 
+        }
+        
+    }
+
+    def fizzBuzz(input: Int){
+        if (input % 3 == 0 && input % 5 == 0){
+            println("FizzBuzz")    
+        }else if (input % 3 == 0){
+            println("Fizz")
+        }else if (input % 5 == 0){
+            println("Buzz")
+        }else{
+            println("not fizz or buzz")
+        }
+    }
+}
